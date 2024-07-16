38327224e5a2844b895ebbb88809d5a2ff139e7f
Gabriela de Araujo Poock <138500369+gabipoock@users.noreply.github.com>
2024-07-16 08:28:11 -0300

    Add files via upload

---
+++ aula2.py
@@ -0,0 +1,4 @@
+primeiro = int(input('Digiteum número: '))
+segundo = int(input('Digite outro número '))
+soma = primeiro + segundo
+print('Asoma dos números é: ', soma)
---
+++ aula3.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+soma = n1 + n2
+print('O primeiro valor digitado foi {} e o segundo valor digitado foi {}, a soma desses valores é {}.'.format(n1, n2, soma))
